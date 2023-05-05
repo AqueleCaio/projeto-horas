1751fc43ebf5a98e1e0f34d2efdab40dfe1d5310
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-05-05 16:29:57 -0300

    .

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ let minutes = date.getMinutes();
 
 // Estilo da página para quando estiver de tarde, entre 12:00 e 17:00
 if (hours > 12 && hours < 17){
-    document.body.style.backgroundColor = '#5BFFDF';
-    info_hours.innerText = `Agora são ${hours}:${minutes} da Tarde`;
+    document.body.style.backgroundColor = 'rgb(43 146 249)';
+    info_hours.innerText = `Agora são exatamente ${hours}:${minutes} da Tarde`;
     img.setAttribute('src', 'imagens/railway-gea24f6fdf_1280.jpg');
 } 
 
 // Estilo da página para quando estiver de noite, entre 17:00 e 5:00
 else if (hours > 17 && hours < 5){
     document.body.style.backgroundColor = '#0B0125';
-    info_hours.innerText = `Agora são ${hours}:${minutes} da Noite`;
+    info_hours.innerText = `Agora são exatamente ${hours}:${minutes} da Noite`;
     img.setAttribute('src', 'imagens/sky-g7c9547511_1280.jpg');
 } 
 
 // Estilo da página para quando estiver de manha, entre 5:00 e 12:00
 else if (hours > 5 && hours < 12){
     document.body.style.backgroundColor = '#FCFF24';
-    info_hours.innerText = `Agora são ${hours}:${minutes} da Manha`;
+    info_hours.innerText = `Agora são exatamente ${hours}:${minutes} da Manha`;
     img.setAttribute('src', 'imagens/trees-gc0a5a8c23_1280.jpg');
 }
 
@@ -34,19 +34,19 @@ switch(hours){
     // Estilo da página para quando for de manha, entre 5:00 e 12:00
     case hours >= 5 && hours <= 12:
         document.body.style.backgroundColor = '#FCFF24';
-        info_hours.innerText = `Agora são ${hours}:${minutes} da Porra`;
+        info_hours.innerText = `Agora são exatamente ${hours}:${minutes} da Porra`;
         img.setAttribute('src', 'imagens/trees-gc0a5a8c23_1280.jpg');
     
 
     case hours >= 12 && hours <= 17: 
         document.body.style.backgroundColor = '#5BFFDF';
-        info_hours.innerText = `Agora são ${hours}:${minutes} da Tarde`;
+        info_hours.innerText = `Agora são exatamente ${hours}:${minutes} da Tarde`;
         img.setAttribute('src', 'imagens/railway-gea24f6fdf_1280.jpg');
     
 
     case  hours >= 17 && hours <= 5:
         document.body.style.backgroundColor = '#0B0125';
-        info_hours.innerText = `Agora são ${hours}:${minutes} da Noite`;
+        info_hours.innerText = `Agora são exatamente ${hours}:${minutes} da Noite`;
         img.setAttribute('src', 'imagens/sky-g7c9547511_1280.jpg');
 }
 
